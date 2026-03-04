76b913ccf7da9ca5a9e8a4eeb4b0b84f45e498bd
ashraful <ahsan.ashraful@adndiginet.com>
2026-03-04 11:24:27 +0600

    Apply CORS

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database import models
 from database.database import engine
 from routers import post
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(post.router)
@@ -15,3 +16,18 @@ async def root():
 models.Base.metadata.create_all(bind=engine)
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)   
